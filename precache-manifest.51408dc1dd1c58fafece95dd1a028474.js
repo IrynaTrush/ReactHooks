1fc3ad8b08ed0247d56b432ce13777368945ccae
Iryna <irynka1071994@gmail.com>
2020-04-15 00:14:51 +0300

    Updates

---
+++ precache-manifest.51408dc1dd1c58fafece95dd1a028474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d215432d9974081be1ffe7c6fc25acd",
+    "url": "/ReactHooks/index.html"
+  },
+  {
+    "revision": "9df7fcb25406d756c002",
+    "url": "/ReactHooks/static/css/main.b2b73074.chunk.css"
+  },
+  {
+    "revision": "2ecef596528f06c9fb6f",
+    "url": "/ReactHooks/static/js/2.e886327e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactHooks/static/js/2.e886327e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df7fcb25406d756c002",
+    "url": "/ReactHooks/static/js/main.2377f444.chunk.js"
+  },
+  {
+    "revision": "28fdbbc66a403de62303",
+    "url": "/ReactHooks/static/js/runtime-main.533fe8a5.js"
+  }
+]);
